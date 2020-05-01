f42848eb69326b10691523394f4eeb4ff72c2b21
simohn <schauppinger@gmail.com>
2020-05-01 11:18:01 +0200

    Camera added

--- RaspberryPi/Camera.py
+++ RaspberryPi/Camera.py
@@ -1,31 +1,19 @@
-# Code written by Simon Schauppenlehner
-# Last change: 22.06.2019
-
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 import numpy as np
-import time
 
-
-def motion_detector():
-
-    camera = PiCamera()
-    camera.resolution = (1088, 720)
-    #camera.rotation = 180
-
-    camera_array = PiRGBArray(camera)
-
-    print("[INFO] warming up...")
-    time.sleep(1)
-
-    camera.capture(camera_array, format="bgr")
+class Camera:
+        
+    def __init__(self):
+        self.camera = PiCamera()
+        self.camera.resolution = (1088, 720)
+        #camera.rotation = 180
+        self.camera_array = PiRGBArray(camera)
     
-    print(camera_array.array.shape)
-    print(camera_array.array.dtype)
     
-    camera_array.truncate(0)
-
-
-if __name__ == "__main__":
-    motion_detector()
-
+    def capture(self):
+        self.camera_array.truncate(0)
+        self.camera.capture(self.camera_array, format="bgr")
+        
+    def saveImage(self):
+        
